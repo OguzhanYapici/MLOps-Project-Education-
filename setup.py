d32028b8b23800943b2a42c3ee4fd6b91ee870a1
OguzhanYapici <106562234+OguzhanYapici@users.noreply.github.com>
2025-03-28 18:47:30 +0000

    setup kuruldu

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Ozz",
+    author_email="oguzhanyapicisoftware@gmail.com",
+    packages=find_packages()
+)
